902dc79c4947f9e1767cd37404789a4cbd827d9d
Mauricio de Azevedo Sousa Neto <mazevedos@minsait.com>
2023-09-11 16:07:29 -0300

    Início do projeto

---
+++ src/main/java/com/matchpointecv/matchpointecv/MatchPointeCvApplication.java
@@ -0,0 +1,13 @@
+package com.matchpointecv.matchpointecv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchPointeCvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchPointeCvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matchpointecv/matchpointecv/MatchPointeCvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matchpointecv.matchpointecv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchPointeCvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

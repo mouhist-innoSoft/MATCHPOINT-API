af3c1bfa2f58968570da8ca6fac1e51567833e2e
Mauricio de Azevedo Sousa Neto <mazevedos@minsait.com>
2023-09-13 22:51:01 -0300

    Configuração

--- src/main/java/com/matchpointecv/matchpointecv/MatchPointeCvApplication.java
+++ src/main/java/com/matchpointecv/matchpointecv/MatchPointeCvApplication.java
@@ -2,10 +2,8 @@ package com.matchpointecv.matchpointecv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
-//@Profile("swagger")
 public class MatchPointeCvApplication {
 
 	public static void main(String[] args) {

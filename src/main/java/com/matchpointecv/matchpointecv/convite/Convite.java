335bdff935387deca827707344903f947d498e6e
Mauricio Neto <mauricioazvedo@gmail.com>
2023-09-17 22:45:34 -0300

    FEAT: Retirada de importação não utilizada

--- src/main/java/com/matchpointecv/matchpointecv/convite/Convite.java
+++ src/main/java/com/matchpointecv/matchpointecv/convite/Convite.java
@@ -6,7 +6,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
